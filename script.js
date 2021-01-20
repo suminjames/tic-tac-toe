70513e99a7fa47ce758102bd33d32769d35cd260
suminjames <51308695+suminjames@users.noreply.github.com>
2021-01-20 10:08:15 +0545

    Refactor (#1)
    * code refactor in progress
    
    * refactor done

--- script.js
+++ script.js
@@ -5,18 +5,22 @@ var playerOne = document.querySelector('.playerOne');
 var playerTwo = document.querySelector('.playerTwo');
 var retryBtn = document.querySelector('.retryBtn');
 
+var player = {'X': 'Player One', 'O': 'Player Two'};
 var playerOneSign = "X";
 var playerTwoSign = "O";
-var filledCount = 0;
-var playerSign;
+var moveMadeCount = 0;
+var playerInputSign;
 var win = false;
 
 var winner = document.querySelector('.winner');
 var winnerName = winner.querySelector('.winnerName');
 var draw = document.querySelector('.draw');
 
+var maxMove = 9;
+var minWinnerCheckMove = 4;
+
 // toggle turn to show whose turn is next
-function toggleTurn(showPlayer, hidePlayer) {
+function togglePlayerActiveTurn(showPlayer, hidePlayer) {
     var activePlayer = showPlayer.querySelector('.activeCircle');
     var waitingPlayer = hidePlayer.querySelector('.activeCircle');
     if (activePlayer.classList.contains('invisible')) {
@@ -26,29 +30,27 @@ function toggleTurn(showPlayer, hidePlayer) {
 }
 
 // to show which player sign should be added to the box
-function playerTurn() {
-    if (filledCount % 2 == 0) {
-        playerSign = playerOneSign;
-        toggleTurn(playerOne, playerTwo);
+function changePlayerInputSign() {
+    if (moveMadeCount % 2 == 0) {
+        playerInputSign = playerOneSign;
+        togglePlayerActiveTurn(playerOne, playerTwo);
     } else {
-        playerSign = playerTwoSign;
-        toggleTurn(playerTwo, playerOne);
+        playerInputSign = playerTwoSign;
+        togglePlayerActiveTurn(playerTwo, playerOne);
     }
 }
 
 // reset board
 function resetBoard() {
-    retryBtn.classList.remove('d-none');
-
     retryBtn.addEventListener('click', function () {
         boxes.forEach(function (box) {
             box.innerHTML = "";
             box.classList.remove('checked');
         });
 
-        filledCount = 0;
+        moveMadeCount = 0;
 
-        playerTurn();
+        changePlayerInputSign();
 
         retryBtn.classList.add('d-none');
 
@@ -65,8 +67,22 @@ function resetBoard() {
     });
 }
 
+// remove active circle or player turn circle after game is completed
+function removeActiveStatus() {
+    document.querySelectorAll('.activeCircle').forEach(function (circle) {
+        circle.classList.add('invisible');
+    });
+}
+
+// show retry button
+function showRetryButton() {
+    retryBtn.classList.remove('d-none');
+    removeActiveStatus();
+    resetBoard();
+}
+
 // make the boxes as checked so that they can not be clicked
-function checkedBoxes() {
+function checkRemainingBoxes() {
     boxes.forEach(function (box) {
         if (!box.classList.contains('checked')) {
             box.classList.add('checked');
@@ -74,74 +90,65 @@ function checkedBoxes() {
     });
 }
 
-// remove active circle or player turn circle after game is completed
-function removeActiveStatus() {
-    document.querySelectorAll('.activeCircle').forEach(function (circle) {
-        circle.classList.add('invisible');
-    })
-}
-
-// declare winner(show the winner name)
-function declareWinner(player) {
-    winner.classList.remove('d-none');
-    winnerName.innerHTML = player;
-
-    removeActiveStatus()
-}
-
 // logic to see if there is a win
-function checkWinner(player) {
+function checkWinningPattern() {
     var winningPattern = [[0, 1, 2], [0, 4, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [2, 4, 6], [3, 4, 5], [6, 7, 8]];
 
     winningPattern.forEach(function (pattern) {
         if (boxes[pattern[0]].innerHTML == boxes[pattern[1]].innerHTML && boxes[pattern[0]].innerHTML == boxes[pattern[2]].innerHTML && boxes[pattern[0]].innerHTML != "") {
-
-            declareWinner(player);
-
-            checkedBoxes();
-
-            resetBoard();
-
             win = true
         }
     });
 
 }
 
+// declare winner(show the winner name)
+function declareWinner(playerSign) {
+    winner.classList.remove('d-none');
+    winnerName.innerHTML = player[playerSign] + ' (' + playerSign + ')';
+}
+
+// check winner after certain move
+function checkWinner() {
+    if (moveMadeCount > minWinnerCheckMove) {
+        checkWinningPattern();
+
+        if (!win && moveMadeCount == maxMove) {
+            draw.classList.remove('d-none');
+            showRetryButton();
+            return false;
+        } else if (win) {
+            declareWinner(playerInputSign);
+            checkRemainingBoxes();
+            showRetryButton();
+            return false;
+        }
+    }
+}
+
+// handle box click event
+function handleClick(box) {
+    box.addEventListener('click', function () {
+        if (!box.classList.contains('checked')) {
+
+            box.innerHTML = playerInputSign;
+            box.classList.add('checked');
+
+            moveMadeCount += 1;
+
+            checkWinner();
+
+            changePlayerInputSign();
+        }
+
+    });
+}
+
 // start game
 function startGame() {
     boxes.forEach(function (box) {
-
-        box.addEventListener('click', function () {
-            if (!box.classList.contains('checked')) {
-
-                playerTurn();
-
-                box.innerHTML = playerSign;
-
-                if (playerSign == playerOneSign) {
-                    toggleTurn(playerTwo, playerOne);
-                } else {
-                    toggleTurn(playerOne, playerTwo);
-                }
-
-                box.classList.add('checked');
-
-                filledCount += 1;
-
-                if (filledCount > 2) {
-                    checkWinner(playerSign);
-
-                    if (!win && filledCount == 9) {
-                        draw.classList.remove('d-none');
-                        removeActiveStatus();
-                        resetBoard();
-                    }
-                }
-            }
-
-        });
-
+        changePlayerInputSign();
+        handleClick(box)
     });
 }
 
